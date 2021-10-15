5708aabefdf1514d8d606260c692f78083c0fa7d
Aymeric <aymeric@wiselabs.net>
2021-10-15 13:50:47 +1100

    :tada: Adds atlantis style

---
+++ assets/generate-index.ps1
@@ -0,0 +1,15 @@
+﻿Import-Module Microsoft.PowerShell.Utility;
+
+$array = [System.Collections.ArrayList]::new();
+
+Get-ChildItem -Path . -Filter *.png | 
+Foreach-Object {
+    $hash = @{}
+    $hash.Add("filename" ,$_.Name);
+    $hash.Add("name" ,[io.path]::GetFileNameWithoutExtension($_.Name).Replace('-', ' '));
+
+    $array.Add($hash);
+}
+
+$result = ConvertTo-Json $array
+Set-Content 'index.json' $result
