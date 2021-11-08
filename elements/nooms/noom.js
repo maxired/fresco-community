6b4a13804161743575752dbcaf18295256dd1b3e
Maxence Dalmais <maxence@fres.co>
2021-11-08 11:02:21 +0100

    additions

--- elements/nooms/noom.js
+++ elements/nooms/noom.js
@@ -1,4 +1,4 @@
-console.log('maxire dnoom')
+
 const nooms = [
     { src: 'https://fresco-upload-prod2.s3.amazonaws.com/public/JAiBJ3KWLTgzSL8yJYGue.png' },
     { src: 'https://fresco-upload-prod2.s3.amazonaws.com/public/j1oKUJyAqNXsnbYWqiDA0.png' },
@@ -11,9 +11,42 @@ const nooms = [
     { src: 'https://fresco-upload-prod2.s3.amazonaws.com/public/8-WNIqL5Gme8cQ3hnaDFQ.png' },
     { src: 'https://fresco-upload-prod2.s3.amazonaws.com/public/esPcTMZzL9sgqJYZeLVDX.png'}
 ]
+const noomDiv = document.querySelector('#noom');
 
- const img = document.querySelector('#noom');
+const Img = (src) => `<img src="${src}" />`
+noomDiv.innerHTML = Img(nooms[0].src)
 
- const index = Math.floor(Math.random() * nooms.length);
+fresco.onReady(function () {
+    function render() {
+        let value = fresco.element.state['sliderConfigurable'];
+        const indexes = []
+       
+        while(value>10) {
+            indexes.unshift(10);
+            value-=10;
+        }
+        indexes.unshift(value);
+        console.log('maxired indexes', { indexes })
+        noomDiv.innerHTML = indexes.map((index) => Img(nooms[index -1].src)).join('')
+    }
 
- img.src = nooms[index].src
+    const defaultState = {
+        sliderConfigurable: 1,
+    };
+
+    const elementConfig = {
+        title: 'Configurables',
+        toolbarButtons: [{
+            title: 'Noom value',
+            ui: { type: 'slider', min: 1, max: 60 },
+            property: 'sliderConfigurable',
+        },
+        ]
+    };
+
+    fresco.onStateChanged(function () {
+        render()
+    });
+    fresco.initialize(defaultState, elementConfig);
+    render()
+});
