5c014fe24e796426e17642811f8d9c2ad30a5db6
Maxence Dalmais <maxence@fres.co>
2021-11-08 17:54:30 +0100

    :sparkles: added hole

---
+++ elements/nooms/hole.js
@@ -0,0 +1,42 @@
+
+const bc = new BroadcastChannel('noom_channel');
+
+fresco.onReady(function () {
+    
+    bc.onmessage = (ev) => {
+        if (ev.data.action === 'onStateChanged') {
+            if (!fresco.element?.transform) {
+                return
+            }
+            const left = fresco.element.transform.position.x -  fresco.element.transform.size.x / 2
+            const right = fresco.element.transform.position.x +  fresco.element.transform.size.x / 2
+            const top = fresco.element.transform.position.y -  fresco.element.transform.size.y / 2
+            const bottom = fresco.element.transform.position.y +  fresco.element.transform.size.y / 2
+          
+            const { x, y } = ev.data.payload.transform.position;
+            console.log('maxired onStateChanged')
+            if( 
+                x > left && x < right && 
+                y > top &&  y < bottom
+            ) {
+        
+                fresco.send({
+                    type: 'extension/out/redux',
+                    payload: {
+                        senderId: fresco.element.id,
+                        action: {
+                            userId: undefined,
+                            type: 'REMOVE_ITEMS',
+                            payload: {
+                                "diagramId":"noomlab",
+                                "itemIds":[ev.data.payload.id]
+                            }
+                        }
+                    }
+                })
+            }
+           
+        }
+    }
+});
+
