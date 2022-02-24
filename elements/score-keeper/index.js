786231c7e3de89e7f5cb279a332c173fe7aec3ad
Maxence Dalmais <maxence@fres.co>
2022-02-24 11:23:10 +0100

    :sparkles: send info to live feed

--- elements/score-keeper/index.js
+++ elements/score-keeper/index.js
@@ -16,12 +16,13 @@ const bc = new BroadcastChannel('fresco-community-score-keeper_channel');
 bc.onmessage = (ev) => {
   if(ev.data.action === 'enter') {
       const nextScore = Number.parseInt(fresco.element.state.score, 10) + 1;
-      fresco.setState({ score: nextScore })
-      render()
-  }
+      fresco.setState({ score: nextScore });
+      fresco.setParticipantLiveFeedItem(`${fresco.element.state.icon} ${nextScore}`, { background: '#ffb136', color: 'white'});
+      render();
+    }
 }
 
 const render = () => {
-    document.querySelector('.score--icon').innerText = fresco.element.state.icon
-    document.querySelector('.score--value').innerText = fresco.element.state.score
-}
+    document.querySelector('.score--icon').innerText = fresco.element.state.icon;
+    document.querySelector('.score--value').innerText = fresco.element.state.score;
+};
