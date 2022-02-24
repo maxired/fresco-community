28a68e520207e43b28dc5489a8ef5b1cd47c07cd
Maxence Dalmais <maxence@fres.co>
2022-02-24 13:05:06 +0100

    :sparkles: allow to customize score-keeper

--- elements/score-keeper/index.js
+++ elements/score-keeper/index.js
@@ -5,10 +5,32 @@ fresco.onReady(function () {
         render()
     });
     
+    const elementConfig = {
+        title: 'Score',
+        toolbarButtons: [
+            {
+                title: 'Color',
+                ui: { type: 'color' },
+                property: 'color'
+            },{
+                title: 'Color',
+                ui: { type: 'color' },
+                property: 'backgroundColor'
+            },
+            {
+                title: 'String',
+                ui: { type: 'string' },
+                property: 'icon'
+            }
+        ]
+    };
+
     fresco.initialize({
         score: '0',
-        icon: '🌱'
-    }, { title: 'Score' });
+        icon: '🌱',
+        color: '#ffffff',
+        backgroundColor: '#ffb136'
+    }, elementConfig);
     render()
 });
 
@@ -17,12 +39,15 @@ bc.onmessage = (ev) => {
   if(ev.data.action === 'enter') {
       const nextScore = Number.parseInt(fresco.element.state.score, 10) + 1;
       fresco.setState({ score: nextScore });
-      fresco.setParticipantLiveFeedItem(`${fresco.element.state.icon} ${nextScore}`, { background: '#ffb136', color: 'white'});
+      fresco.setParticipantLiveFeedItem(`${fresco.element.state.icon} ${nextScore}`, { background: fresco.element.state.backgroundColor, color: fresco.element.state.color });
       render();
     }
 }
 
 const render = () => {
+    const score = document.querySelector('.score');
+    score.style.setProperty('background-color', fresco.element.state.backgroundColor);
+    score.style.setProperty('color', fresco.element.state.color);
     document.querySelector('.score--icon').innerText = fresco.element.state.icon;
     document.querySelector('.score--value').innerText = fresco.element.state.score;
 };
