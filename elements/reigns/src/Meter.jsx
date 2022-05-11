83c8846cf613eaa6682546403577a60c0112cbfc
Aymeric <aymeric@wiselabs.net>
2022-05-11 15:01:04 +1000

    :tada: Moves all game related information into the json

--- elements/reigns/src/Meter.jsx
+++ elements/reigns/src/Meter.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export const Meter = ({ src, percent }) => {
-    return (
-      <div className="meter">
-        <div className="meter__percent" style={{ height: percent + "%" }} />
-        <img src={src} />
-      </div>
-    );
-  };
-  
+  const assetsUrl = useSelector((state) => state.game.definition?.assetsUrl);
+
+  return (
+    <div className="meter">
+      <div className="meter__percent" style={{ height: percent + "%" }} />
+      <img src={`${assetsUrl}/${src}`} />
+    </div>
+  );
+};
--- elements/reigns/src/Question.jsx
+++ elements/reigns/src/Question.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export const Question = ({ card }) => {
-    return (
+  const assetsUrl = useSelector((state) => state.game.definition?.assetsUrl);
+  return (
       <div className="block question">
         <div className="question__image">
-          <img src={`/${card.bearer}.png`} />
+          <img src={`${assetsUrl}/${card.bearer}.png`} />
           {card.bearer}
         </div>
         <div className="question__text">{card.card}</div>
