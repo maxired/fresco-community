18ede6b43c2d5a335ca0f2b0d043257caaa611b1
Stafford Williams <stafford.williams@gmail.com>
2022-05-12 17:58:48 +1000

    🎨 pr feedback

--- elements/reigns/src/features/game/gameSlice.test.ts
+++ elements/reigns/src/features/game/gameSlice.test.ts
@@ -1,30 +1,33 @@
 import { answerNo, answerYes, gameReducer, initialState, startGame } from "./gameSlice";
 import { Card, GameDefinition, GameState } from "./types";
 
-describe("gameReducer", () => {
-  const state: GameState = {
+const getState = ()  => ({
     ...initialState,
+    selectedCard: { card: "a card"},
     definition: {
-      cards: [{ card: "a card" } as Card],
+      cards: [{ card: "another card" } as Card],
       stats: [{ value: 0, icon: "icon" }],
     } as GameDefinition,
-  };
+  } as GameState
+);
+
+describe("gameReducer", () => {
   describe("startGame", () => {
-    it("should select next card", () => {
-      const result = gameReducer(state, startGame());
-      expect(result.selectedCard).not.toBeNull();
+    it("should select a card", () => {
+      const result = gameReducer(getState(), startGame());
+      expect(result.selectedCard?.card).toBe("another card")
     });
   });
   describe("answerYes", () => {
-    it("should select next card", () => {
-      const result = gameReducer(state, answerYes());
-      expect(result.selectedCard).not.toBeNull();
+    it("should select a card", () => {
+      const result = gameReducer(getState(), answerYes());
+      expect(result.selectedCard?.card).toBe("another card")
     });
   });
   describe("answerNo", () => {
-    it("should select next card", () => {
-      const result = gameReducer(state, answerNo());
-      expect(result.selectedCard).not.toBeNull();
+    it("should select a card", () => {
+      const result = gameReducer(getState(), answerNo());
+      expect(result.selectedCard?.card).toBe("another card")
     });
   });
 });
