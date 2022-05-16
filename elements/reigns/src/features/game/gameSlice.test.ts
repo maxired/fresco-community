b42a030d3c2e10341c5a2afe0f471d8765fa9c37
Stafford Williams <stafford.williams@gmail.com>
2022-05-16 19:09:25 +1000

    ✅ add subcutaneous test

--- elements/reigns/src/features/game/gameSlice.test.ts
+++ elements/reigns/src/features/game/gameSlice.test.ts
@@ -20,51 +20,89 @@ const getState = (cards?: Card[]) =>
     } as GameDefinition,
   } as GameState);
 
-describe("gameReducer", () => {
-  describe("startGame", () => {
-    it("should select a card", () => {
-      const result = gameReducer(getState(), startGame());
-      expect(result.selectedCard?.card).toBe("another card");
+describe("gameSlice", () => {
+  describe("gameReducer", () => {
+    describe("startGame", () => {
+      it("should select a card", () => {
+        const result = gameReducer(getState(), startGame());
+        expect(result.selectedCard?.card).toBe("another card");
+      });
     });
-  });
-  describe("answerYes", () => {
-    it("should select a card", () => {
-      const result = gameReducer(getState(), answerYes());
-      expect(result.selectedCard?.card).toBe("another card");
+    describe("answerYes", () => {
+      it("should select a card", () => {
+        const result = gameReducer(getState(), answerYes());
+        expect(result.selectedCard?.card).toBe("another card");
+      });
     });
-  });
-  describe("answerNo", () => {
-    it("should select a card", () => {
-      const result = gameReducer(getState(), answerNo());
-      expect(result.selectedCard?.card).toBe("another card");
+    describe("answerNo", () => {
+      it("should select a card", () => {
+        const result = gameReducer(getState(), answerNo());
+        expect(result.selectedCard?.card).toBe("another card");
+      });
+    });
+
+    describe("initialize", () => {
+      it("should enter error state if validation fails", () => {
+        const result = gameReducer(
+          {} as GameState,
+          initializeGame.fulfilled(getState([]).definition, "", "")
+        );
+        expect(result.phase).toBe(GamePhase.ERROR);
+      });
     });
   });
 
-  describe("initialize", () => {
-    it("should enter error state if validation fails", () => {
-      const result = gameReducer(
-        {} as GameState,
-        initializeGame.fulfilled(getState([]).definition, "", "")
-      );
-      expect(result.phase).toBe(GamePhase.ERROR);
+  describe("setFlags", () => {
+    it("should set multiple flags", () => {
+      const flags = setFlags({}, [
+        { key: "chapter3", value: "true" },
+        { key: "queen_killed", value: "false" },
+      ]);
+      expect(flags.chapter3).toBe("true");
+      expect(flags.queen_killed).toBe("false");
+    });
+
+    it("should leave existing flags", () => {
+      const flags = setFlags({ chapter3: "true" }, [
+        { key: "queen_killed", value: "false" },
+      ]);
+      expect(flags.chapter3).toBe("true");
     });
   });
-});
 
-describe("setFlags", () => {
-  it("should set multiple flags", () => {
-    const flags = setFlags({}, [
-      { key: "chapter3", value: "true" },
-      { key: "queen_killed", value: "false" },
-    ]);
-    expect(flags.chapter3).toBe("true");
-    expect(flags.queen_killed).toBe("false");
-  });
+  describe("conditions", () => {
+    it("can step through conditions", () => {
+      const card1 = {
+        conditions: "dragon_killed==false",
+        card: "Kill the dragon?",
+        yes_custom: "dragon_killed=true",
+        weight: 1,
+      } as Card;
 
-  it("should leave existing flags", () => {
-    const flags = setFlags({ chapter3: "true" }, [
-      { key: "queen_killed", value: "false" },
-    ]);
-    expect(flags.chapter3).toBe("true");
+      const card2 = {
+        conditions: "dragon_killed==true",
+        card: "Have a feast?",
+        weight: 1,
+      } as Card;
+
+      const gameState = {
+        definition: {
+          cards: [card1, card2],
+          deathMessage: "You died",
+          assetsUrl: "test",
+          stats: [{ value: 0, icon: "icon" }],
+        },
+        flags: {},
+      } as GameState;
+
+      const turn1 = gameReducer(gameState, startGame());
+      expect(turn1.selectedCard?.card).toBe("Kill the dragon?");
+
+      const turn2 = gameReducer(turn1, answerNo());
+      expect(turn2.selectedCard?.card).toBe("Kill the dragon?");
+
+      const turn3 = gameReducer(turn2, answerYes());
+      expect(turn3.selectedCard?.card).toBe("Have a feast?");
+    });
   });
 });
