5c051bb031320108fafa8a42d0ad60a6ec795097
Stafford Williams <stafford.williams@gmail.com>
2022-05-24 12:14:21 +1000

    🎨 pr feedback

--- elements/reigns/src/features/host/persistIsMounted.ts
+++ elements/reigns/src/features/host/persistIsMounted.ts
@@ -7,11 +7,8 @@ export const persistIsMounted = throttle((isMounted: boolean) => {
   const sdk = getSdk();
   if (isMounted) {
     sdk.storage.set(IS_MOUNTED_TABLE, sdk.localParticipant.id, true);
-    console.log("sending is mounted for", sdk.localParticipant.id);
+    console.log("Sending is mounted for", sdk.localParticipant.id);
   } else {
-    const mounted = sdk.storage.get(IS_MOUNTED_TABLE, sdk.localParticipant.id);
-    if (mounted) {
-      sdk.storage.remove(IS_MOUNTED_TABLE, mounted.id);
-    }
+    sdk.storage.remove(IS_MOUNTED_TABLE, sdk.localParticipant.id);
   }
 }, 1000);
