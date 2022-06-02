16d6842751cde8e590d80bf82cdad4eac88c8f52
Stafford Williams <stafford.williams@gmail.com>
2022-06-02 12:18:51 +1000

    ✅ redux mocks

--- elements/reigns/src/store.ts
+++ elements/reigns/src/store.ts
@@ -3,14 +3,13 @@ import { gameReducer } from "./features/game/gameSlice";
 import { reducer as hostReducer } from "./features/host/hostSlice";
 import { reducer as votingReducer } from "./features/voting/votingSlice";
 
-const store = configureStore({
-  reducer: {
-    game: gameReducer,
-    host: hostReducer,
-    voting: votingReducer,
-  },
-});
+export const createStore = () =>
+  configureStore({
+    reducer: {
+      game: gameReducer,
+      host: hostReducer,
+      voting: votingReducer,
+    },
+  });
 
-export default store;
-
-export type AppState = ReturnType<typeof store.getState>;
+export type AppState = ReturnType<ReturnType<typeof createStore>["getState"]>;
