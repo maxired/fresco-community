58580c4d0a742dc6d555ee5903ea53581520aad8
Stafford Williams <stafford.williams@gmail.com>
2022-06-14 13:48:41 +1000

    ✅ split tests

--- elements/reigns/src/features/game/selectAnswer.test.ts
+++ elements/reigns/src/features/game/selectAnswer.test.ts
@@ -43,8 +43,9 @@ describe("selectAnswer", () => {
       .retrieve();
     expect(result.round).toBe(2);
   });
-  it("should end game if a card reduces a stat to zero", () => {
-    const result = new Game()
+  describe('game end', () => {
+    const chooseAnswerThatEndsGame = () => {
+      const result = new Game()
       .answerYes(
         createGameState(undefined, {
           selectedCard: createCard({
@@ -55,23 +56,34 @@ describe("selectAnswer", () => {
         })
       )
       .retrieve();
-    expect(result.phase).toBe(GamePhase.ENDED);
-    expect(result.flags).toStrictEqual({});
-    expect(result.selectedCard).toBe(null);
-  });
-  it("should not end game if a stat is already zero and not updated by a card", () => {
-    const result = new Game()
-      .answerYes(
-        createGameState(undefined, {
-          selectedCard: createCard({
-            yes_stat1: -1,
-            yes_stat2: 0,
-          }),
-          stats: [4, 0],
-          phase: GamePhase.STARTED,
-        })
-      )
-      .retrieve();
-    expect(result.phase).toBe(GamePhase.STARTED);
-  });
+      return result;
+    }
+    it("should end game if a card reduces a stat to zero", () => {
+      const result = chooseAnswerThatEndsGame();
+      expect(result.phase).toBe(GamePhase.ENDED);
+    });
+    it("should clear flags", () => {
+      const result = chooseAnswerThatEndsGame();
+      expect(result.flags).toStrictEqual({});
+    });
+    it("should clear selected card", () => {
+      const result = chooseAnswerThatEndsGame();
+      expect(result.selectedCard).toBe(null);
+    })
+    it("should not end game if a stat is already zero and not updated by a card", () => {
+      const result = new Game()
+        .answerYes(
+          createGameState(undefined, {
+            selectedCard: createCard({
+              yes_stat1: -1,
+              yes_stat2: 0,
+            }),
+            stats: [4, 0],
+            phase: GamePhase.STARTED,
+          })
+        )
+        .retrieve();
+      expect(result.phase).toBe(GamePhase.STARTED);
+    });
+  })
 });
