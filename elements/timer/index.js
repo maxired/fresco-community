02ec7e4030b7c9cbcb4223934a52db01aab2cbd6
arnaudonline <arnaud.beaugeon@live.fr>
2022-06-14 14:17:54 +0200

    [~] Add inputs and set state of them

--- elements/timer/index.js
+++ elements/timer/index.js
@@ -1,5 +1,13 @@
 const button = document.getElementById("timer-button");
 
+function valueForm(e) {
+  let minutes = parseFloat(document.getElementById('minutes').value);
+  let seconds = parseFloat(document.getElementById('seconds').value) / 60;
+
+  let formDuration = minutes + seconds;
+  fresco.setState({ duration: formDuration });
+}
+
 function toTime(milliseconds) {
   if (milliseconds < 0) {
     milliseconds = 0;
@@ -61,34 +69,32 @@ function toggleTimer() {
 fresco.onReady(function () {
   const urlParams = new URLSearchParams(window.location.search);
   const duration = urlParams.get("duration");
-
+  
   const defaultState = {
     duration: duration ? parseFloat(duration, 2) : 5,
     startedAt: null,
   };
 
+  
   const elementConfig = {
     title: "Timer",
-    toolbarButtons: [
-      {
-        title: "Duration (min)",
-        icon: '<i class="fa fa-users" />',
-        ui: { type: "slider", min: 1, max: 60 },
-        property: "duration",
-      },
-    ],
+    toolbarButtons: [],
   };
-
+  
   fresco.onStateChanged(function () {
     if (!fresco.element.state.startedAt) {
       stopTimer();
     } else {
       stopTimer();
       targetTime =
-        fresco.element.state.startedAt +
-        fresco.element.state.duration * 60 * 1000;
+      fresco.element.state.startedAt +
+      fresco.element.state.duration * 60 * 1000;
       startTimer(targetTime, new Date().getTime());
     }
+
+    const actualTime = (fresco.element.state.duration * 60);
+    document.getElementById('minutes').value = Math.floor(actualTime / 60);
+    document.getElementById('seconds').value = Math.floor(actualTime % 60);
 
     if (!fresco.localParticipant.permission.canEdit) {
       button.setAttribute("disabled", true);
