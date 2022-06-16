ab821c68b07b96077183404c46027de5b3a82c5d
arnaudonline <arnaud.beaugeon@live.fr>
2022-06-16 13:15:03 +0200

    [~] All buttons is OK

--- elements/timer/index.js
+++ elements/timer/index.js
@@ -1,11 +1,13 @@
 const button = document.getElementById("timer-button");
 
-function valueForm(e) {
-  let minutes = parseFloat(document.getElementById('minutes').value);
-  let seconds = parseFloat(document.getElementById('seconds').value) / 60;
+function valueForm() {
+  const minutes = parseFloat(document.getElementById("minutes").value);
+  const seconds = parseFloat(document.getElementById("seconds").value) / 60;
 
-  let formDuration = minutes + seconds;
+  const formDuration = minutes + seconds;
   fresco.setState({ duration: formDuration });
+
+  return formDuration;
 }
 
 function toTime(milliseconds) {
@@ -26,6 +28,7 @@ function toTime(milliseconds) {
 
 let interval = null;
 let targetTime = null;
+let paused = false;
 
 function stopTimer() {
   button.classList.remove("button--done");
@@ -36,9 +39,21 @@ function stopTimer() {
   }
 }
 
+function pauseTimer() {
+  clearInterval(interval);
+  const timePause = targetTime;
+  targetTime = (timePause - new Date().getTime()) / 60 / 1000;
+  fresco.setState({ duration: targetTime, startedAt: null });
+}
+
+function resetTimer() {
+  targetTime = 10.0;
+  interval = null;
+  fresco.setState({ duration: targetTime, startedAt: null });
+}
+
 function startTimer(targetTime, now) {
   button.innerText = toTime(targetTime - 1 - now);
-
   interval = setInterval(() => {
     const timeRemaining = targetTime - new Date().getTime();
     button.innerText = toTime(timeRemaining);
@@ -50,6 +65,7 @@ function startTimer(targetTime, now) {
       interval = null;
     }
   }, 1000);
+  console.log(interval);
 }
 
 function toggleTimer() {
@@ -69,32 +85,31 @@ function toggleTimer() {
 fresco.onReady(function () {
   const urlParams = new URLSearchParams(window.location.search);
   const duration = urlParams.get("duration");
-  
+
   const defaultState = {
     duration: duration ? parseFloat(duration, 2) : 5,
     startedAt: null,
   };
 
-  
   const elementConfig = {
     title: "Timer",
     toolbarButtons: [],
   };
-  
+
   fresco.onStateChanged(function () {
     if (!fresco.element.state.startedAt) {
       stopTimer();
     } else {
       stopTimer();
       targetTime =
-      fresco.element.state.startedAt +
-      fresco.element.state.duration * 60 * 1000;
+        fresco.element.state.startedAt +
+        fresco.element.state.duration * 60 * 1000;
       startTimer(targetTime, new Date().getTime());
     }
 
-    const actualTime = (fresco.element.state.duration * 60);
-    document.getElementById('minutes').value = Math.floor(actualTime / 60);
-    document.getElementById('seconds').value = Math.floor(actualTime % 60);
+    const actualTime = fresco.element.state.duration * 60;
+    document.getElementById("minutes").value = Math.floor(actualTime / 60);
+    document.getElementById("seconds").value = Math.floor(actualTime % 60);
 
     if (!fresco.localParticipant.permission.canEdit) {
       button.setAttribute("disabled", true);
@@ -110,4 +125,5 @@ fresco.onReady(function () {
       button.setAttribute("disabled", true);
     }
   }
+  render();
 });
