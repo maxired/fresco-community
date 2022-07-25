8cd4b1caa5f17e2f315689c06d775b6aa7903fed
Stafford Williams <stafford.williams@gmail.com>
2022-07-25 16:40:40 +1000

    ♻ extract textfit

---
+++ elements/reigns/src/useTextFit.ts
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from "react";
+import textfit from "textfit";
+
+export const useTextFit = (text: string | undefined) => {
+  const ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (!ref.current || !text) {
+      return;
+    }
+    ref.current.innerHTML = `${text}`; // textfit will modify the dom node. We don't want React to also modify it's content to prevent conflict
+    textfit(ref.current, {
+      alignHoriz: false,
+      alignVert: true,
+      reProcess: true,
+      multiLine: true,
+      maxFontSize: 1000,
+    });
+  }, [ref, text]);
+  return ref;
+};
