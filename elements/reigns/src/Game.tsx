29e3cbfc9c62596bd67db97aaa2ba3c3b5b062f4
Maxence Dalmais <maxence@fres.co>
2022-08-08 13:40:48 +0200

    :lipstick: fix starting screen style

--- elements/reigns/src/Game.tsx
+++ elements/reigns/src/Game.tsx
@@ -81,13 +81,11 @@ export const Game = () => {
 
   if (phase === GamePhase.NOT_STARTED) {
     return (
-      <div className="game-half first-half">
-        <div className="end">
-          <div className="end__message">{gameDefinition?.gameName}</div>
-          {isHost && <button onClick={doRestartGame}>Start game</button>}
-          {!isHost && <div>Waiting for host to start...</div>}
-        </div>
-      </div>
+      <NotStartScreen
+        gameDefinition={gameDefinition}
+        isHost={isHost}
+        doRestartGame={doRestartGame}
+      />
     );
   }
 
@@ -168,6 +166,32 @@ const getEndMessage = (
 
   return gameDefinition.deathMessage;
 };
+function NotStartScreen({
+  gameDefinition,
+  isHost,
+  doRestartGame,
+}: {
+  gameDefinition: GameDefinition | null;
+  isHost: string | boolean | undefined;
+  doRestartGame: () => void;
+}) {
+  const messageRef = useTextFit(gameDefinition?.gameName);
+
+  return (
+    <div className="game-half first-half">
+      <div className="end">
+        <div className="end__message" ref={messageRef} />
+        {isHost && (
+          <button className="end__restart_button" onClick={doRestartGame}>
+            Start game
+          </button>
+        )}
+        {!isHost && <div>Waiting for host to start...</div>}
+      </div>
+    </div>
+  );
+}
+
 function EndScreen({
   gameDefinition,
   currentStats,
